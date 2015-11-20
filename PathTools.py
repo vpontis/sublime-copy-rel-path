a3bdce92808ac048680072bb1ed96684f117e2d9
Victor Pontis <victor@benchling.com>
2015-11-20 12:11:14 -0800

    Add back ticks command

--- PathTools.py
+++ PathTools.py
@@ -42,3 +42,18 @@ class CopyRelativePathWithLineNumCommand(sublime_plugin.TextCommand):
 
     def is_enabled(self):
         return bool(self.view.file_name() and len(self.view.file_name()) > 0)
+
+
+class CopyRelativePathWithLineNumAndTicksCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        relative_path, found = get_relative_path_to_git_repo(self.view.file_name())
+        line_num = self.view.rowcol(self.view.sel()[0].begin())[0] + 1
+        relative_path_with_line_num = '`%s:%s`' % (relative_path, line_num)
+        if found:
+            sublime.set_clipboard(relative_path_with_line_num)
+        else:
+            sublime.status_message("Could not find parent git repo. Copied file path: %s" % relative_path_with_line_num)
+        return
+
+    def is_enabled(self):
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
